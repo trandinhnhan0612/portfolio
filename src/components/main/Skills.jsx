2eb8a58b4faab957f9fb8fcf5fa351e35b1d7cc9
trandinhnhan0612 <trandinhnhan9958@gmail.com>
2023-04-09 16:33:55 +0700

    make it better

--- src/components/main/Skills.jsx
+++ src/components/main/Skills.jsx
@@ -1,23 +1,22 @@
 import React from "react";
+import "../../App.css";
 
 const Skills = () => {
   return (
     <section id="skill">
-      <div className="container">
-        <div className="p-3">
-          <h1
-            data-aos="fade-down"
-            data-aos-duration="1500"
-            className="text-gray-500 text-center tracking-[20px] text-[28px] md:text-[32px] font-[700]"
-          >
-            MY SKILLS
-          </h1>
-          <div className="ml-[20px] md:mx-auto mt-8 grid grid-cols-1 md:grid-cols-2 gap-y-8 md:max-w-[900px]">
-            <div
-              data-aos="fade-right"
-              data-aos-duration="1800"
-              className="flex gap-x-3 justify-center items-center w-[300px] md:w-[360px] h-[110px] border border-solid border-gray-300 p-3 rounded-lg hover:bg-gray-200 hover:bg-opacity-10"
-            >
+      <div className="p-3">
+        <h1
+          data-aos="fade-down"
+          data-aos-duration="1500"
+          className="text-gray-500 text-center tracking-[20px] text-[28px] md:text-[32px] font-[700]"
+        >
+          MY SKILLS
+        </h1>
+        {/* <div className="slider w-full flex justify-center items-center flex-col mt-8"> */}
+        {/* <div className="wrraper flex items-center justify-center "> */}
+        <div className="slider overflow-hidden min-w-full bg-[#0f172a]">
+          <div className="slide_track">
+            <div className="flex justify-center items-center py-6 px-[34px] m-4 bg-[#161b22] max-w-[360px] rounded-2xl gap-x-3">
               <figure className="w-[50px] h-[50px]">
                 <img src="/src/assets/images/frontend.jpeg" alt="" />
               </figure>
@@ -31,11 +30,7 @@ const Skills = () => {
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-left"
-              data-aos-duration="1800"
-              className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] border border-solid border-gray-300 p-3 rounded-lg hover:bg-gray-200 hover:bg-opacity-10"
-            >
+            <div className="flex justify-center items-center py-6 px-[34px] m-4 bg-[#161b22] max-w-[360px] rounded-2xl gap-x-3">
               <figure className="w-[50px] h-[50px]">
                 <img src="/src/assets/images/react.png" alt="" />
               </figure>
@@ -48,11 +43,7 @@ const Skills = () => {
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-right"
-              data-aos-duration="1800"
-              className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] border border-solid border-gray-300 p-3 rounded-lg hover:bg-gray-200 hover:bg-opacity-10"
-            >
+            <div className="flex justify-center items-center py-6 px-[34px] m-4 bg-[#161b22] max-w-[360px] rounded-2xl gap-x-3">
               <figure className="w-[50px] h-[50px]">
                 <img src="/src/assets/images/jquery.png" alt="" />
               </figure>
@@ -65,11 +56,7 @@ const Skills = () => {
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-left"
-              data-aos-duration="1800"
-              className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] border border-solid border-gray-300 p-3 rounded-lg hover:bg-gray-200 hover:bg-opacity-10"
-            >
+            <div className="flex justify-center items-center py-6 px-[34px] m-4 bg-[#161b22] max-w-[360px] rounded-2xl gap-x-3">
               <figure className="w-[50px] h-[50px]">
                 <img src="/src/assets/images/tailwind.png" alt="" />
               </figure>
@@ -82,79 +69,207 @@ const Skills = () => {
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-right"
-              data-aos-duration="1800"
-              className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] border border-solid border-gray-300 p-3 rounded-lg hover:bg-gray-200 hover:bg-opacity-10"
-            >
+            <div className="flex justify-center items-center py-6 px-[34px] m-4 bg-[#161b22] max-w-[360px] rounded-2xl gap-x-3">
               <figure className="w-[50px] h-[50px]">
-                <img src="/src/assets/images/bootstrap.png" alt="" />
+                <img src="/src/assets/images/frontend.jpeg" alt="" />
               </figure>
               <div>
                 <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
-                  Bootstrap
+                  HTML/CSS(SCSS)/JAVASCRIPT
                 </h5>
                 <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
-                  Knowledge about CSS library bootstrap
+                  Knowledge about front end language: HTML, CSS(SCSS),
+                  Javascript
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-left"
-              data-aos-duration="1800"
-              className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] border border-solid border-gray-300 p-3 rounded-lg hover:bg-gray-200 hover:bg-opacity-10"
-            >
+            <div className="flex justify-center items-center py-6 px-[34px] m-4 bg-[#161b22] max-w-[360px] rounded-2xl gap-x-3">
               <figure className="w-[50px] h-[50px]">
-                <img src="/src/assets/images/material.png" alt="" />
+                <img src="/src/assets/images/react.png" alt="" />
               </figure>
               <div>
                 <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
-                  Material UI
+                  REACTJS
                 </h5>
                 <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
-                  Knowledge about CSS library material ui
+                  Knowledge about reactjs, redux, redux thunk, redux toolkit
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-right"
-              data-aos-duration="1800"
-              className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] border border-solid border-gray-300 p-3 rounded-lg hover:bg-gray-200 hover:bg-opacity-10"
-            >
+            <div className="flex justify-center items-center py-6 px-[34px] m-4 bg-[#161b22] max-w-[360px] rounded-2xl gap-x-3">
               <figure className="w-[50px] h-[50px]">
-                <img src="/src/assets/images/styledCom.png" alt="" />
+                <img src="/src/assets/images/jquery.png" alt="" />
               </figure>
               <div>
                 <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
-                  STYLED-COMPONENTS
+                  JQUERY
                 </h5>
                 <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
-                  Knowledge about CSS library styled-components
+                  Basic knowledge about JQuery
                 </p>
               </div>
             </div>
-            <div
-              data-aos="fade-left"
-              data-aos-duration="1800"
-              className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] border border-solid border-gray-300 p-3 rounded-lg hover:bg-gray-200 hover:bg-opacity-10"
-            >
+            <div className="flex justify-center items-center py-6 px-[34px] m-4 bg-[#161b22] max-w-[360px] rounded-2xl gap-x-3">
               <figure className="w-[50px] h-[50px]">
-                <img src="/src/assets/images/responsive.png" alt="" />
+                <img src="/src/assets/images/tailwind.png" alt="" />
               </figure>
               <div>
                 <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
-                  RESPONSIVE WEB DESIGN
+                  TAILWINDCSS
                 </h5>
                 <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
-                  Basic knowledge about responsove web design
+                  Knowledge about CSS library Tailwindcss
                 </p>
               </div>
             </div>
           </div>
         </div>
+        {/* </div> */}
+        {/* </div> */}
       </div>
     </section>
   );
 };
 
 export default Skills;
+
+{
+  /* <div className="ml-[18px] md:mx-auto mt-8 grid grid-cols-1 md:grid-cols-2 gap-y-8 md:max-w-[900px]">
+  <div
+    data-aos="fade-right"
+    data-aos-duration="1800"
+    className="flex gap-x-3 justify-center items-center w-[300px] md:w-[360px] h-[110px] bg-[#161b22] p-3 rounded-lg"
+  >
+    <figure className="w-[50px] h-[50px]">
+      <img src="/src/assets/images/frontend.jpeg" alt="" />
+    </figure>
+    <div>
+      <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
+        HTML/CSS(SCSS)/JAVASCRIPT
+      </h5>
+      <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
+        Knowledge about front end language: HTML, CSS(SCSS), Javascript
+      </p>
+    </div>
+  </div>
+  <div
+    data-aos="fade-left"
+    data-aos-duration="1800"
+    className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] bg-[#161b22] p-3 rounded-lg"
+  >
+    <figure className="w-[50px] h-[50px]">
+      <img src="/src/assets/images/react.png" alt="" />
+    </figure>
+    <div>
+      <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
+        REACTJS
+      </h5>
+      <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
+        Knowledge about reactjs, redux, redux thunk, redux toolkit
+      </p>
+    </div>
+  </div>
+  <div
+    data-aos="fade-right"
+    data-aos-duration="1800"
+    className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] bg-[#161b22] p-3 rounded-lg"
+  >
+    <figure className="w-[50px] h-[50px]">
+      <img src="/src/assets/images/jquery.png" alt="" />
+    </figure>
+    <div>
+      <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
+        JQUERY
+      </h5>
+      <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
+        Basic knowledge about JQuery
+      </p>
+    </div>
+  </div>
+  <div
+    data-aos="fade-left"
+    data-aos-duration="1800"
+    className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] bg-[#161b22] p-3 rounded-lg"
+  >
+    <figure className="w-[50px] h-[50px]">
+      <img src="/src/assets/images/tailwind.png" alt="" />
+    </figure>
+    <div>
+      <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
+        TAILWINDCSS
+      </h5>
+      <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
+        Knowledge about CSS library Tailwindcss
+      </p>
+    </div>
+  </div>
+  <div
+    data-aos="fade-right"
+    data-aos-duration="1800"
+    className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] bg-[#161b22] p-3 rounded-lg"
+  >
+    <figure className="w-[50px] h-[50px]">
+      <img src="/src/assets/images/bootstrap.png" alt="" />
+    </figure>
+    <div>
+      <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
+        Bootstrap
+      </h5>
+      <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
+        Knowledge about CSS library bootstrap
+      </p>
+    </div>
+  </div>
+  <div
+    data-aos="fade-left"
+    data-aos-duration="1800"
+    className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] bg-[#161b22] p-3 rounded-lg"
+  >
+    <figure className="w-[50px] h-[50px]">
+      <img src="/src/assets/images/material.png" alt="" />
+    </figure>
+    <div>
+      <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
+        Material UI
+      </h5>
+      <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
+        Knowledge about CSS library material ui
+      </p>
+    </div>
+  </div>
+  <div
+    data-aos="fade-right"
+    data-aos-duration="1800"
+    className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] bg-[#161b22] p-3 rounded-lg"
+  >
+    <figure className="w-[50px] h-[50px]">
+      <img src="/src/assets/images/styledCom.png" alt="" />
+    </figure>
+    <div>
+      <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
+        STYLED-COMPONENTS
+      </h5>
+      <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
+        Knowledge about CSS library styled-components
+      </p>
+    </div>
+  </div>
+  <div
+    data-aos="fade-left"
+    data-aos-duration="1800"
+    className="flex gap-x-3 items-center w-[300px] md:w-[360px] h-[110px] bg-[#161b22] p-3 rounded-lg"
+  >
+    <figure className="w-[50px] h-[50px]">
+      <img src="/src/assets/images/responsive.png" alt="" />
+    </figure>
+    <div>
+      <h5 className="text-smallTextColor text-[14px] md:text-[16px] font-[700]">
+        RESPONSIVE WEB DESIGN
+      </h5>
+      <p className="text-gray-400 text-[13px] md:text-[15px] font-[400]">
+        Basic knowledge about responsove web design
+      </p>
+    </div>
+  </div>
+</div>; */
+}
